c0c27644fc90f2e0b545a13b49825ea8cdbbdf86
EdoFro <35700575+EdoFro@users.noreply.github.com>
2021-11-14 22:59:36 -0300

    implemented swing.edt for showtextmessage

--- Tutorial-o-Matic/src/main/groovy/ToM_ui.groovy
+++ Tutorial-o-Matic/src/main/groovy/ToM_ui.groovy
@@ -75,22 +75,25 @@ class ToM_ui{
     )
     
     def static showTextMessage(msg, lapseTime){
-        def win = swing.dialog(
-            undecorated        : true,
-            modal              : false,
-            owner              : ui.frame,
-            locationRelativeTo : ui.frame,
-            location           : [(ui.frame.location.x + 100) as int, (ui.frame.location.y + ui.frame.height - 150) as int],
-            //defaultCloseOperation: JFrame.DO_NOTHING_ON_CLOSE,
-            background         : new Color (240, 240, 240, 150),
-            pack               : true
-        ){
-            label(
-                text       : msg,
-                font       : new Font("Agency FB", Font.BOLD, 48),
-                foreground : Color.blue,
-                border     : new EmptyBorder(0, 15, 10, 15),
-            )
+        def win
+        swing.edt{
+            win = dialog(
+                undecorated        : true,
+                modal              : false,
+                owner              : ui.frame,
+                locationRelativeTo : ui.frame,
+                location           : [(ui.frame.location.x + 100) as int, (ui.frame.location.y + ui.frame.height - 150) as int],
+                //defaultCloseOperation: JFrame.DO_NOTHING_ON_CLOSE,
+                background         : new Color (240, 240, 240, 150),
+                pack               : true
+            ){
+                label(
+                    text       : msg,
+                    font       : new Font("Agency FB", Font.BOLD, 48),
+                    foreground : Color.blue,
+                    border     : new EmptyBorder(0, 15, 10, 15),
+                )
+            }
         }
         def timer = new Timer()
         timer.runAfter(lapseTime){
